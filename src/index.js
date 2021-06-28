be9de47dc0d46367b0365cc49d035e23cbbb46e3
ccnarvaez <ccnarvaez90@gmail.com>
2021-06-28 19:03:55 -0400

    Firebase initialized

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React, {Component } from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-// import './assets/data/firebase';
 import  {Header, Menu} from './components/Components';
+import firebase from 'firebase';
 
-
-
-
-
-// import reportWebVitals from './reportWebVitals';
-
-
-
+// Render app
 class Index extends Component {
   render() {
     return (
@@ -24,6 +17,30 @@ class Index extends Component {
 }
 
 ReactDOM.render(<Index />, document.getElementById('root'));
+
+// Firebase 
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBT4zMkjZfic41viFbALSDNLBsiygMEBZw",
+  authDomain: "scl016-burguer-queen.firebaseapp.com",
+  databaseURL: "https://scl016-burguer-queen-default-rtdb.firebaseio.com",
+  projectId: "scl016-burguer-queen",
+  storageBucket: "scl016-burguer-queen.appspot.com",
+  messagingSenderId: "63205452563",
+  appId: "1:63205452563:web:c1ff3a1e0eae8f1b34c8e6",
+  measurementId: "G-Q4CBRE3XX2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+
+// import reportWebVitals from './reportWebVitals';
+
+
+
+
 
 
 
